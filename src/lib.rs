3a22b18ab7c37ffa665762ecbccbb8264c7820ef
Bruno Palermo <bpalermo@pinterest.com>
2022-12-01 14:59:42 -0800

    feat: initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use hyper::{http::StatusCode, Client};
+use std::error::Error;
+
+pub async fn health_check(
+    host: String,
+    port: u16,
+    path: String,
+    http_code: u16,
+) -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+    let uri = format!("http://{host}:{port}{path}").parse().unwrap();
+    let res = client.get(uri).await;
+
+    res.map(|res| {
+        let status_code = res.status();
+        if status_code != StatusCode::from_u16(http_code).unwrap() {
+            return Err(());
+        }
+        return Ok(())
+    })
+    .map_err(|e| return e)
+    .ok();
+
+    Ok(())
+}
